d685572793be5904a508c769a6eb2ff0e64bf149
Pallabi Sahu <pallabisahu0408@gmail.com>
2026-07-14 10:38:23 +0530

    Day-4 Program

---
+++ ReverseArray.py
@@ -0,0 +1,9 @@
+arr=list(map(int, input().split()))
+left=0
+right=len(arr)-1
+while(left<right):
+    arr[left],arr[right]=arr[right],arr[left]
+    left +=1
+    right -=1
+print("The reversed array is :",arr)
+

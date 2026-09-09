df345f41b822fe3c3f561d3e88da9acdc540a278
Pallabi Sahu <pallabisahu0408@gmail.com>
2026-09-09 23:03:20 +0530

    Day-20 Program

---
+++ LinkedList.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self,data):#Constructor which wil be called automatically when an object is created
+        self.data=data
+        self.next=None
+node1=Node(10)#object created
+node2=Node(20)
+node3=Node(30)
+node4=Node(40)
+node5=Node(50)
+node1.next=node2
+node2.next=node3
+node3.next=node4
+node4.next=node5
+current=node1
+while current is not None:
+    print(current.data, end="->")
+    current=current.next
+print(None)
